6c8c8c6509a2de2aa3cf056227167347d75be8a9
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-27 03:39:42 -0800

    Fixed: missing a function declaration

--- fact.h
+++ fact.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+FILE *fileOpener(const char *);
 unsigned long dtSwitcher(const char *, unsigned long);
 void fileFactorizer(FILE *, char *, size_t);
 void numFactorizer(unsigned long);
